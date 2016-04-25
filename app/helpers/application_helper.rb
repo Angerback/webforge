a06af107aa2ebe0db7b7f7649a0ef85cb47598ec
sebastian <sebastian.meneses.n@usach.cl>
2016-04-24 22:12:09 -0300

    Correjido problema con las alertas. Se tuvo que elminar notif para corregirlo.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,23 @@
 module ApplicationHelper
   def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
+    if flash_type == "success"
+      return "alert-success"
+    end
+    if flash_type == "error"
+      return "alert-danger"
+    end
+    if flash_type == "alert"
+      return "alert-warning"
+    end
+    if flash_type == "notice"
+      return "alert-info"
+    end
+
   end
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}",role: "alert") do
               concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
               concat message
             end)
