55b3bbaabbed332bd9f3c467e9517a2ebdaae9f8
sebastian <sebastian.meneses.n@usach.cl>
2016-06-27 16:03:59 -0400

    Peticiones API Angerback/webforge#53 Angerback/webforge#54 Angerback/webforge#55 Angerback/webforge#56 Angerback/webforge#57

--- app/controllers/api/v2/answers_controller.rb
+++ app/controllers/api/v2/answers_controller.rb
@@ -20,4 +20,56 @@ class Api::V2::AnswersController < API::V2::ApiController
        end
 
   end
+
+    def update
+    if @answer = Answer.find_by(question_id: params[:question_id], test_id: params[:test_id])
+      if @answer.update(question_id: params[:question_id], test_id: params[:test_id], )
+        if @test.update(grade: params[:grade])
+        render json: {
+          outcome: "Respuesta actualizada."
+        }
+      else
+        render json: {
+          outcome: "Respuesta no actualizada."
+        }
+      end
+    else
+      render json: {
+        outcome: "Error en respuesta."
+      }
+    end
+  end
+
+  def show
+    if params[:question_id]
+      if @answer = Answer.find_by(question_id: params[:question_id], test_id: params[:test_id], alternative: params[:alternative])
+        render json: {
+          outcome: "Ya existe respuesta"
+        }
+      else
+        render json: {
+          outcome: "No existe respuesta"
+        }
+      end
+    else
+      render json: {
+        outcome: "Problemas con respuesta"
+      }
+    end
+  end
+
+  def showTest
+    if params[:test_id]
+        if @answers = Answer.where( test_id: params[:test_id])
+        else
+            render json: {
+                outcome: "Respuestas no encontradas"
+            }
+        end
+    else
+        render json: {
+            outcome: "Problemas con respuestas"
+        }
+    end
+  end
 end
