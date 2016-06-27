2a03406f60357594583f6cb8f0f265a6da3531e5
nelsonjerezv <nelsonjerezv>
2016-06-27 12:07:29 -0400

    fix update

--- app/controllers/api/v2/answers_controller.rb
+++ app/controllers/api/v2/answers_controller.rb
@@ -1,77 +1,78 @@
 class Api::V2::AnswersController < API::V2::ApiController
 
-  def create
-	  if params[:question_id]
-		@answer = Answer.new(question_id: params[:question_id], test_id: params[:test_id], alternative: params[:alternative])
+	def create
+		if params[:question_id]
+			@answer = Answer.new(question_id: params[:question_id], test_id: params[:test_id], alternative: params[:alternative])
 
-		if @answer.save
-		  render json: {
-				outcome: "Respuesta registrada exitosamente."
-			 }
+			if @answer.save
+				render json: {
+					outcome: "Respuesta registrada exitosamente."
+				}
+			else
+				render json: {
+					outcome: "Error al registrar respuesta."
+				}
+			end
 		else
-		  render json: {
-				outcome: "Error al registrar respuesta."
-			 }
+			render json: {
+				outcome: "Falta especificar pregunta."
+			}
 		end
-	  else
-		render json: {
-			outcome: "Falta especificar pregunta."
-		 }
-	   end
 
-  end
-
-  def update
-	if @answer = Answer.find_by(question_id: params[:question_id], test_id: params[:test_id])
-
-	  if @answer.update(question_id: params[:question_id], test_id: params[:test_id], )
-	  	if @test.update(grade: params[:grade])
-		render json: {
-		  outcome: "Respuesta actualizada."
-		}
-	  else
-		render json: {
-		  outcome: "Respuesta no actualizada."
-		}
-	  end
-	else
-	  render json: {
-		outcome: "Error en respuesta."
-	  }
 	end
-  end
 
-  def show
-	if params[:question_id]
-	  if @answer = Answer.find_by(question_id: params[:question_id], test_id: params[:test_id], alternative: params[:alternative])
-		render json: {
-		  outcome: "Ya existe respuesta"
-		}
-	  else
-		render json: {
-		  outcome: "No existe respuesta"
-		}
-	  end
-	else
-	  render json: {
-		outcome: "Problemas con respuesta"
-	  }
+	def update
+		if @answer = Answer.find_by(question_id: params[:question_id], test_id: params[:test_id])
+
+			if @answer.update(question_id: params[:question_id], test_id: params[:test_id], )
+				if @test.update(grade: params[:grade])
+					render json: {
+						outcome: "Respuesta actualizada."
+					}
+				else
+					render json: {
+						outcome: "Respuesta no actualizada."
+					}
+				end
+			else
+				render json: {
+					outcome: "Error en respuesta."
+				}
+			end
+		end
 	end
-  end
 
-  def showTest
-  	if params[:test_id]
-  		if @answers = Answer.where( test_id: params[:test_id])
-  		else
-  			render json: {
-  				outcome: "Respuestas no encontradas"
-  			}
-  		end
-  	else
-  		render json: {
-  			outcome: "Problemas con respuestas"
-  		}
-  	end 
-  end
+	def show
+		if params[:question_id]
+			if @answer = Answer.find_by(question_id: params[:question_id], test_id: params[:test_id], alternative: params[:alternative])
+				render json: {
+					outcome: "Ya existe respuesta"
+				}
+			else
+				render json: {
+					outcome: "No existe respuesta"
+				}
+			end
+		else
+			render json: {
+				outcome: "Problemas con respuesta"
+			}
+		end
+	end
+
+	def showTest
+		if params[:test_id]
+			if @answers = Answer.where( test_id: params[:test_id])
+			else
+				render json: {
+					outcome: "Respuestas no encontradas"
+				}
+			end
+		else
+			render json: {
+				outcome: "Problemas con respuestas"
+			}
+		end 
+	end
 
 end
